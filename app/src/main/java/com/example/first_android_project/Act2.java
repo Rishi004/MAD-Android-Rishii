0876b29f1171efed81b66f183bd32cdfe4a86f32
Rishi004 <rishireiko@gmail.com>
2021-08-11 10:50:16 +0530

    First-Android

---
+++ app/src/main/java/com/example/first_android_project/Act2.java
@@ -0,0 +1,14 @@
+package com.example.first_android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act2);
+    }
+}
